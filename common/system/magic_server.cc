c2f3e38bbd9760559084677df97f071e2e9ed553
warsier <787012293@qq.com>
2020-12-05 20:26:55 -0500

    temp

--- common/system/magic_server.cc
+++ common/system/magic_server.cc
@@ -100,16 +100,16 @@ UInt64 MagicServer::Magic_unlocked(thread_id_t thread_id, core_id_t core_id, UIn
       case SIM_CMD_MHZ_GET:
          return getFrequency(arg0);
       case SIM_PIMPROF_BBL_START: // [Yizhou]
-         Sim()->PIMProfBBLStart(Sim()->getThreadManager()->getCurrentThread()->getId(), arg0, arg1);
+         Sim()->PIMProfBBLStart(Sim()->getCoreManager()->getCurrentCoreID(), arg0, arg1);
          return 0;
       case SIM_PIMPROF_BBL_END: // [Yizhou]
-         Sim()->PIMProfBBLEnd(Sim()->getThreadManager()->getCurrentThread()->getId(), arg0, arg1);
+         Sim()->PIMProfBBLEnd(Sim()->getCoreManager()->getCurrentCoreID(), arg0, arg1);
          return 0;
       case SIM_PIMPROF_OFFLOAD_START: // [Yizhou]
-         Sim()->PIMProfOffloadStart(Sim()->getThreadManager()->getCurrentThread()->getId(), arg0, arg1);
+         Sim()->PIMProfOffloadStart(Sim()->getCoreManager()->getCurrentCoreID(), arg0, arg1);
          return 0;
       case SIM_PIMPROF_OFFLOAD_END: // [Yizhou]
-         Sim()->PIMProfOffloadEnd(Sim()->getThreadManager()->getCurrentThread()->getId(), arg0, arg1);
+         Sim()->PIMProfOffloadEnd(Sim()->getCoreManager()->getCurrentCoreID(), arg0, arg1);
          return 0;
       default:
          LOG_ASSERT_ERROR(false, "Got invalid Magic %lu, arg0(%lu) arg1(%lu)", cmd, arg0, arg1);
